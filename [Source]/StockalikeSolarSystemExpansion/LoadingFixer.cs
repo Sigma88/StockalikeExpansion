d173e0688d8d5d898d7396f58059a8f6fa21cd34
Sigma88 <sigmamodexpansions@outlook.com>
2017-05-23 14:29:14 +0200

    add loading screen fixer

---
+++ [Source]/StockalikeSolarSystemExpansion/LoadingFixer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+using Kopernicus;
+using Random = System.Random;
+using SASSPlugin;
+
+
+namespace SASSxPlugin
+{
+    [KSPAddon(KSPAddon.Startup.Instantly, true)]
+    class FixLoadingScreen : SASSPlugin.FixLoadingScreen
+    {
+        void Awake()
+        {
+            bool useSASSxLoadingScreen = true;
+
+            foreach (ConfigNode Settings in GameDatabase.Instance.GetConfigNodes("SASSxLoadingScreen"))
+            {
+                if (Settings.HasValue("useSASSxLoadingScreen"))
+                {
+                    NumericParser<bool> userSetting = new NumericParser<bool>();
+                    userSetting.SetFromString(Settings.GetValue("useSASSxLoadingScreen"));
+                    if (userSetting.value == false)
+                    {
+                        useSASSxLoadingScreen = false;
+                    }
+                    if (userSetting.value == true)
+                    {
+                        useSASSxLoadingScreen = true;
+                        break;
+                    }
+                }
+            }
+
+            if (useSASSxLoadingScreen)
+            {
+                newLoadingScreen = new SASSxLoadingScreen();
+            }
+        }
+    }
+
+    class SASSxLoadingScreen : NewLoadingScreen
+    {
+        public void UpdateScreen()
+        {
+            Debug.Log("SigmaLog: LOADING SCREENS INTERCEPTED BY SASSx");
+        }
+    }
+}
